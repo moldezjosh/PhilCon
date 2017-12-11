324572b92705774fbf859dca639d2b94e82457e2
Joshua Mark Moldez <j.markmoldez@gmail.com>
2017-12-12 06:57:16 +0800

    updated files

--- projects/PhilCon/www/js/javascript.js
+++ projects/PhilCon/www/js/javascript.js
@@ -1,13 +1,16 @@
+
+// show side menu
 function openNav() {
-    document.getElementById("mySidenav").style.width = "80%";
+    document.getElementById("mySidenav").style.width = "100%";
 }
 
+// close the side menu
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
 
-
+// show dropdown contents
 function myFunction(e) {
   var str1 = "myDropdown";
   var d = str1.concat(e);
@@ -24,22 +27,6 @@ if (!event.target.matches('.dropbtn')) {
    var openDropdown = dropdowns[i];
    if (openDropdown.classList.contains('show')) {
      openDropdown.classList.remove('show');
-   }
- }
-}
-}
-
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-if (!event.target.matches('.sidenav')) {
-
- var sidenav = document.getElementsByClassName("sidenav");
- var i;
- for (i = 0; i < sidenav.length; i++) {
-   var openSidenav = sidenav[i];
-   if (openSidenav.classList.contains('show')) {
-     openSidenav.classList.remove('show');
    }
  }
 }
